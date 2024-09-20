629c9e9bc3b43b1793ee5b76cd82062608f5142d
Andrew Nguyen <andrew@spacegray.lan>
2024-09-20 13:38:30 -0700

    chunks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import { useEffect, useMemo } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import chunkedEvents from './events-chunked';
+import rrweb from 'rrweb';
+import rrwebPlayer from 'rrweb-player';
+import 'rrweb-player/dist/style.css';
 
 function App() {
+  const events = useMemo(() => {
+    return chunkedEvents
+      .filter(chunk => chunk.rrwebEvents.length)
+      .sort((a, b) => a.rrwebEvents[0].timestamp - b.rrwebEvents[0].timestamp)
+      .reduce((arr, chunk) => [...arr, ...chunk.rrwebEvents], [])
+  }, []);
+
+  useEffect(() => {
+    console.log('events', events);
+    new rrwebPlayer({
+      target: document.getElementById('hello'), // customizable root element
+      props: {
+        events,
+        UNSAFE_replayCanvas: true,
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="hello">
     </div>
   );
 }
